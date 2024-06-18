5f805cf02cf85d06a0b353a0242d01d78b7e883e
Siya Mandal <143694322+siya2910@users.noreply.github.com>
2024-06-18 11:31:51 +0530

    Add files via upload

---
+++ offers.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const bookPartyButton = document.getElementById('book-party');
+
+    bookPartyButton.addEventListener('click', () => {
+        alert('Thank you for choosing SWASTIK for your birthday celebration! Our team will contact you soon.');
+    });
+});
